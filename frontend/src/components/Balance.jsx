720e58f4058917f3ece0c7d69331d2a085d453e1
samKit88 <samuel.girma.cx9896@gmail.com>
2023-11-08 04:41:14 -0800

    component created

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,8 +1,15 @@
+import { useContext } from 'react'
+import { GlobalContext } from '../Context/GlobalState'
+
 const Balance = () => {
+  const { transactions } = useContext(GlobalContext)
+
+  const amounts = transactions.map((t) => t.amount)
+  const totalAmount = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
   return (
     <>
       <h3>Your Balance</h3>
-      <h2>$0.00</h2>
+      <h2>${totalAmount}</h2>
     </>
   )
 }
--- frontend/src/components/TransactionHistory.jsx
+++ frontend/src/components/TransactionHistory.jsx
@@ -3,6 +3,8 @@ import { GlobalContext } from '../Context/GlobalState'
 
 const TransactionHistory = () => {
   const { transactions } = useContext(GlobalContext)
+  const { deleteTransaction } = useContext(GlobalContext)
+
   return (
     <>
       <h3>History</h3>
@@ -10,7 +12,11 @@ const TransactionHistory = () => {
         {transactions.map((t) => (
           <li key={t.id}>
             {' '}
-            {t.text} and {t.amount}
+            {t.text}{' '}
+            <span>
+              {t.amount < 0 ? '-' : '+'}${Math.abs(t.amount)}
+            </span>
+            <button onClick={() => deleteTransaction(t.id)}>delete</button>
           </li>
         ))}
       </ul>
